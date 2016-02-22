b32a1f8f6e614ac3dd1e3dc1017d1020ee7e117f
Simone Bnà <s.bn@cineca.it>
2016-02-22 16:16:54 +0100

    set petsc qsub script working on galileo

--- hpc-numerical-libraries-course/petsc/petsc_qsub_script.sh
+++ hpc-numerical-libraries-course/petsc/petsc_qsub_script.sh
@@ -1,12 +1,16 @@
-#!/bin/bash/
+#!/bin/bash
 #PBS -l walltime=1:00:00
-#PBS -l select=2:ncpus=10:mpiprocs=10    # 2 nodes, 10 procs/node = 20 MPI tasks
+#PBS -l select=1:ncpus=2:mpiprocs=2
 #PBS -o job.out
 #PBS -e job.err
-#PBS -A cineca_cin
+#PBS -A cin_staff
 
-PBS_O_WORKDIR = /galileo/home/sbna0000/
-cd $PBS_O_WORKDIR ! this is the dir where the job was submitted from
+PBS_O_WORKDIR="/galileo/home/userinternal/sbna0000/software/CINECA-SCAI-HPC-courses/hpc-numerical-libraries-course/petsc"
+cd $PBS_O_WORKDIR 
 
-module autoload petsc
-mpirun ./4_petsc_ksp_poisson -da_grid_x 64 -da_grid_y 64
+echo $PWD
+
+module load intel/cs-xe-2015--binary
+module load intelmpi/5.0.2--binary
+module load petsc/3.5.2--intelmpi--5.0.2--binary
+mpirun -np 2 ./4_petsc_ksp_poisson -da_grid_x 64 -da_grid_y 64 -ksp_monitor_true_residual -ksp_view
