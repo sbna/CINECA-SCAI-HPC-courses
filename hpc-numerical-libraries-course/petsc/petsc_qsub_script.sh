a5c906f48641c553176af0b08b8d0272985099fb
Simone Bnà <s.bn@cineca.it>
2016-02-23 15:28:19 +0100

    changed petsc qsub script in order to use petsc 3.6.3 intelmpi 5.1.1 module

--- hpc-numerical-libraries-course/petsc/petsc_qsub_script.sh
+++ hpc-numerical-libraries-course/petsc/petsc_qsub_script.sh
@@ -6,11 +6,19 @@
 #PBS -A cin_staff
 
 PBS_O_WORKDIR="/galileo/home/userinternal/sbna0000/software/CINECA-SCAI-HPC-courses/hpc-numerical-libraries-course/petsc"
-cd $PBS_O_WORKDIR 
+cd ${PBS_O_WORKDIR} 
 
-echo $PWD
+echo ${PBS_O_WORKDIR}
 
-module load intel/cs-xe-2015--binary
-module load intelmpi/5.0.2--binary
-module load petsc/3.5.2--intelmpi--5.0.2--binary
+# modules to be loaded for petsc version 3.5.2
+#module load intel/cs-xe-2015--binary
+#module load intelmpi/5.0.2--binary
+#module load petsc/3.5.2--intelmpi--5.0.2--binary
+
+# modules to be loaded for petsc version 3.6.3
+module load profile/advanced
+module load intel/pe-xe-2016--binary
+module load intelmpi/5.1.1--binary
+module load petsc/3.6.3--intelmpi--5.1.1--binary
+
 mpirun -np 2 ./4_petsc_ksp_poisson -da_grid_x 64 -da_grid_y 64 -ksp_monitor_true_residual -ksp_view
