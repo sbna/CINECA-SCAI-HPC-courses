797d3b45b0cd9b0232aff91a024a0110e75b4c73
Simone Bnà <s.bn@cineca.it>
2016-02-23 15:35:58 +0100

    added petsc load environment script

---
+++ hpc-numerical-libraries-course/petsc/petsc_load_env.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# modules to be loaded for petsc version 3.5.2
+#module load intel/cs-xe-2015--binary
+#module load intelmpi/5.0.2--binary
+#module load petsc/3.5.2--intelmpi--5.0.2--binary
+
+# modules to be loaded for petsc version 3.6.3
+module load profile/advanced
+module load intel/pe-xe-2016--binary
+module load intelmpi/5.1.1--binary
+module load petsc/3.6.3--intelmpi--5.1.1--binary
